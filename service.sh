0e988f5d54af32e0445fdb92a536213f4bfbd0f1
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-06-23 18:07:46 +0000

    e

--- service.sh
+++ service.sh
@@ -1,7 +1,7 @@
 while true; do
   current_time=$(date +%H:%M)
   
-  if [[ "$current_time" > "22:50" || "$current_time" < "02:20" ]]; then
+  if [[ "$current_time" > "22:56" || "$current_time" < "02:20" ]]; then
     su -lp 2000 -c "cmd notification post -S bigtext -t 'Sleep!' 'sleep' 'It is about to sleep now; your phone will power off in 4 minutes.'"
     sleep 180
     su -lp 2000 -c "cmd notification post -S bigtext -t 'Sleep!' 'sleep' 'It is time to sleep now; your phone will power off in 60 seconds. Good night~'"
